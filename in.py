c904d8aea2c2091550fbce6d415d4726e0cf62cc
SohamGunturu <67756303+SohamGunturu@users.noreply.github.com>
2020-07-14 13:31:32 -0400

    Add files via upload

---
+++ in.py
@@ -0,0 +1,11 @@
+def squares(limit):
+    list_1 = []
+    for i in range(1, limit + 1):
+        fol = (i*i)
+        list_1.append(fol)
+        print(list_1)
+        
+        
+squares(4)
+        
+
